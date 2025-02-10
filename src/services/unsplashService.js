726724c9dbffc4529c4ee46ed152ee353604fbb3
shubhamchauhan <shubhamchauhan4488@gmail.com>
2025-02-10 12:29:00 -0500

    refactor: use axios for service call

--- src/services/unsplashService.js
+++ src/services/unsplashService.js
@@ -1,17 +1,28 @@
+import axios from 'axios';
+
 const ACCESS_KEY = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
+const BASE_URL = 'https://api.unsplash.com';
 
-export const fetchImages = async ({ query, page = 1, perPage = 16 }) => {
-  const endpoint = query
-    ? `https://api.unsplash.com/search/photos?query=${encodeURIComponent(
-      query
-    )}&page=${page}&per_page=15&client_id=${ACCESS_KEY}`
-    : `https://api.unsplash.com/photos?page=${page}&per_page=${perPage}&client_id=${ACCESS_KEY}`;
+export const fetchImages = async ({ query = '', page = 1, perPage = 16 }) => {
+  const endpoint = query ? `${BASE_URL}/search/photos` : `${BASE_URL}/photos`;
 
-  const response = await fetch(endpoint);
-  if (!response.ok) {
-    throw new Error('Failed to fetch images');
+  try {
+    const response = await axios.get(endpoint, {
+      params: {
+        query: query || undefined,
+        page,
+        per_page: perPage,
+        client_id: ACCESS_KEY,
+      },
+    });
+
+    const data = response.data;
+    return {
+      data: data.results ? data.results : data,
+      total_pages: data.total_pages || 0,
+    };
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw new Error('Failed to fetch images. Please try again later.');
   }
-
-  const data = await response.json();
-  return  {data: data.results ? data.results : data, total_pages: data?.total_pages || 0};
 };
